f1269a7c8977f7a0ca636b56030583e19bd85ee9
Denis Chernyshov <denis.chernyshov@gloria-jeans.com>
2025-04-09 11:29:26 +0300

    remove svc

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,14 +1,12 @@
 package app
 
 import (
-	"context"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"time"
 
 	"github.com/behummble/1c_exporter/internal/config"
-	"golang.org/x/sys/windows/svc"
 )
 
 type ExporterService struct {
@@ -38,54 +36,9 @@ func New(metricService Metric, config *config.Config, log *slog.Logger) *Exporte
 }
 
 func(s *ExporterService) Run() {
-	//go svc.Run("1C_Programm_Lic_exporter", s)
 	http.Handle("/metrics", s.metricService.Handler())
 	if err := s.server.ListenAndServe(); err != http.ErrServerClosed {
 		s.log.Error(err.Error())
 	}
 
-}
-
-func(s *ExporterService) Execute(args []string, r <-chan svc.ChangeRequest, status chan<- svc.Status) (bool, uint32) {
-
-    const cmdsAccepted = svc.AcceptStop | svc.AcceptShutdown | svc.AcceptPauseAndContinue
-
-	s.log.Info("start service...")
-
-    status <- svc.Status{State: svc.StartPending}
-
-    status <- svc.Status{State: svc.Running, Accepts: cmdsAccepted}
-
-	loop:
-		for {
-			select {
-			case c := <-r:
-				switch c.Cmd {
-				case svc.Interrogate:
-					status <- c.CurrentStatus
-				case svc.Stop, svc.Shutdown:
-					s.log.Info("shutdown service...")
-					s.stop()
-					break loop
-				case svc.Pause:
-					status <- svc.Status{State: svc.Paused, Accepts: cmdsAccepted}
-				case svc.Continue:
-					status <- svc.Status{State: svc.Running, Accepts: cmdsAccepted}
-				default:
-					s.log.Info(fmt.Sprintf("Unexpected service control request #%d", c))
-				}
-			}
-		}
-
-    status <- svc.Status{State: svc.StopPending}
-    return false, 1
-}
-
-
-func(s *ExporterService) stop() {
-	err := s.server.Shutdown(context.Background())
-	if err != nil {
-		s.log.Error(err.Error())
-	}
-	s.metricService.Unregister()
 }
